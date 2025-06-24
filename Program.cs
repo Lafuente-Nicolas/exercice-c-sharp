f70a5cfb2e7f2bd03ae8a038a2a568489f87d0d8
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-24 19:49:54 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Linq;
+
+namespace exercice
+{
+    internal class Program
+    {
+        /*
+        static int Multiplier(int a , int b )
+        {
+            return a * b;
+        }
+        static int Multiplier(int a , int b , int c )
+        {
+            return a * b * c;
+        }
+
+        static int Multiplier(int[] tableau)
+        {
+            int produit = 1;
+            foreach (int n in tableau)
+            {
+                produit = n * produit;
+            }
+            return produit;
+        }*//*
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Multiplier(2, 3)}");
+            Console.WriteLine($"{Multiplier(2, 3, 4)}");
+            int[] tableauOriginal = { 10, 12, 15, 14 };
+            Console.WriteLine($"{Multiplier(tableauOriginal)}");
+        }
+    }
+};
+*/
+        // Exercice 5 : Static ou instance ?
+        /*        void BonjourPersonnalise(string nom)
+                    {
+                        Console.WriteLine($"Bonjour {nom}");
+                    }
+                    static void BonjourGlobal()
+                    {
+                        Console.WriteLine("Bonjour les gens");
+                    }
+                    static void Main(string[] args)
+                    {
+                        Program.BonjourGlobal();
+                        Program program = new Program();
+                        program.BonjourPersonnalise("Alice");
+                    }
+                }
+            };*/
+        static void Main(string[] args)
+        {
+
+        }
+    }
+};
