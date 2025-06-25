15752018ff1c0b928a1da0a9fc10f1a5b853992a
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-25 10:59:38 +0200

    feat: add exercice

--- Program.cs
+++ Program.cs
@@ -80,4 +80,43 @@ namespace exercice
             }
 
         };*/
-    
+
+        /*       static void Main(string[] args)
+               {
+                   int[] tableau = new int[] { 5, 8, 13 };
+
+                   AnalyseNombres(tableau);
+               }
+
+               static void AnalyseNombres(int[] tableau)
+               {
+
+                   bool EstPair(int x)
+                   {
+                       return x % 2 == 0;
+                   }
+
+                   foreach (int nombre in tableau)
+                   {
+
+                       if (EstPair(nombre))
+                       {
+                           Console.WriteLine($"{nombre} est pair");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"{nombre} est impair");
+                       }
+
+
+                   }
+
+               }
+           }
+       };*/
+
+        static void Main(string[] args)
+        {
+        }
+    }
+};
